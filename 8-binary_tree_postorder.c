c8a0b58d7949767a1c02c63b07bbcf53c82ae814
DiegoSusviela <dieguitosus@hotmail.com>
2021-07-19 13:35:43 -0700

    and this should also work

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,10 @@
+#include "binary_trees.h"
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree)
+		return;
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
