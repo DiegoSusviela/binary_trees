2259ff37eb5a132b4e8ae278e1b9e8373c373706
DiegoSusviela <dieguitosus@hotmail.com>
2021-07-19 13:14:22 -0700

    not discading cons

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -2,7 +2,8 @@
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if (node && !hijo_izq(node) && !hijo_der(node))
+    binary_tree_t *nodo = node;
+    if (node && !hijo_izq(nodo) && !hijo_der(nodo))
         return (1);
     return (0);
 }
