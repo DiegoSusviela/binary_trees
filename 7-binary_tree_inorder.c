f12c03989f1686ca2401cc931ece14754e63cd05
DiegoSusviela <dieguitosus@hotmail.com>
2021-07-19 13:33:53 -0700

    didnt change name of things #

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -4,7 +4,7 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (!tree)
 		return;
-	binary_tree_preorder(tree->left, func);
+	binary_tree_inorder(tree->left, func);
 	func(tree->n);
-	binary_tree_preorder(tree->right, func);
+	binary_tree_inorder(tree->right, func);
 }
