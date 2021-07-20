bbfa8523542e5d678cd21491418d28683136e2e9
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-07-20 02:23:02 +0000

    added task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+#include "4-binary_tree_is_leaf.c"
 
 /**
  * binary_tree_leaves - counts the leaves in a binary tree.
@@ -8,5 +9,10 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-
+	if (!tree)
+		return (0);
+	if (binary_tree_is_leaf(tree))
+		return (1);
+	return (binary_tree_leaves(hijo_der(tree)) +
+	binary_tree_leaves(hijo_izq(tree)));
 }
