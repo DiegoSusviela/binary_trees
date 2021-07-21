c2656f6a0b8beef625fddef2bc716c79b48a6f08
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-07-21 18:55:54 +0000

    updated task :: fixed multiple inclusion

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -6,12 +6,11 @@
  * @tree: pointer to the root node of the tree to traverse.
  * @func: pointer to a function to call for each node.
  */
-
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (!tree || !func)
 		return;
-	binary_tree_preorder(tree->left, func);
+	binary_tree_inorder(tree->left, func);
 	func(tree->n);
-	binary_tree_preorder(tree->right, func);
+	binary_tree_inorder(tree->right, func);
 }
--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -6,12 +6,11 @@
  * @tree: pointer to the root node of the tree to traverse.
  * @func: pointer to a function to call for each node.
  */
-
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (!tree || !func)
 		return;
-	binary_tree_preorder(tree->left, func);
-	binary_tree_preorder(tree->right, func);
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
 	func(tree->n);
 }
