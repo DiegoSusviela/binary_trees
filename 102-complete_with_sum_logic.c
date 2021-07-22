f6e33a6b3bc957dbb4755460ce3f1bc927aa5b29
DiegoSusviela <dieguitosus@hotmail.com>
2021-07-22 18:56:11 -0300

    changed function name, in old one

--- 102-complete_with_sum_logic.c
+++ 102-complete_with_sum_logic.c
@@ -38,5 +38,5 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	return (help_complete(tree, 0, binary_tree_size(tree)));
+	return (checker(tree, 0, binary_tree_size(tree)));
 }
